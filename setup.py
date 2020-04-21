537ae934867b746fe7b573c0cb55f8dd76d0d15e
apoyet <32321729+apoyet@users.noreply.github.com>
2020-04-21 09:42:38 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='phd-toolbox',
+setup(name='phd_toolbox',
       version='0.01',
       description='A small toolbox with useful tools for the PhD.',
       url='https://github.com/apoyet/phd-toolbox',
       author='Axel POYET',
       author_email='axel.poyet@cern.ch',
       license='MIT',
-      packages=['phd-toolbox'],
+      packages=['phd_toolbox'],
       zip_safe=False)
