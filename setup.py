5a9f11dc3f5e144a7f055ac6bd63be4707b86521
apoyet <32321729+apoyet@users.noreply.github.com>
2020-04-21 09:33:32 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='phd-toolbox',
+      version='0.01',
+      description='TODO',
+      url='https://github.com/apoyet/phd-toolbox',
+      author='Axel POYET',
+      author_email='axel.poyet@cern.ch',
+      license='MIT',
+      packages=['phd-toolbox'],
+      zip_safe=False)
