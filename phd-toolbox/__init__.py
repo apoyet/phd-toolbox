33af29abe638db50acd084cb9fa634a4976735ae
apoyet <32321729+apoyet@users.noreply.github.com>
2020-04-21 09:36:12 +0200

    Create __init__.py

---
+++ phd-toolbox/__init__.py
@@ -0,0 +1,7 @@
+'''
+This is a simple collection of packages containing some useful tools for the PhD. 
+
+Once installed, you can easily import one of the packages like
+
+from phd-toolbox import maths-tools
+'''
